514d83ec66eabc49214f573b94572c66101ce6f4
Piotr Dobrowolski <admin@tastycode.pl>
2022-02-02 21:12:07 +0100

    webos: add version sync script

---
+++ webos/scripts/sync-version.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const packageInfo = JSON.parse(fs.readFileSync('package.json'));
+const appInfo = JSON.parse(fs.readFileSync('webos-meta/appinfo.json'));
+
+fs.writeFileSync(
+	'webos-meta/appinfo.json',
+	`${JSON.stringify(
+		{
+			...appInfo,
+			version: packageInfo.version,
+		},
+		null,
+		'\t',
+	)}\n`,
+);
